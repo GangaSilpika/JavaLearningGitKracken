ee360905e04aa086686471276cbd24a9b0940db9
Ganga Silpika <swarnasilpika@gmail.com>
2023-08-24 13:46:33 -0500

    Small change in  code
    Added "Execution Completed" print statement

--- Day2Package/Perimeter.java
+++ Day2Package/Perimeter.java
@@ -10,6 +10,7 @@ public class Perimeter {
         System.out.println("Enter breadth of your rectangle");
         int b = sc.nextInt();
         System.out.println("The perimeter of your rectangle is "+ (perimeter(a,b)));
+        System.out.println("Execution completed");
     }
     public static int perimeter(int a, int b){
         return (2*(a+b));

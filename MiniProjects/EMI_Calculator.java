ca07ab1a521736cdfa1e62d9d93da07649447b64
Ganga Silpika <swarnasilpika@gmail.com>
2023-09-05 12:32:52 -0500

    Committed MiniProjects are ammended
    MIniProjects have few changes ammended to them

--- MiniProjects/EMI_Calculator.java
+++ MiniProjects/EMI_Calculator.java
@@ -4,27 +4,27 @@ import java.util.Scanner;
 
 public class EMI_Calculator {
     public static void main(String[] args) {
-        int  LoanAmount, Tenure, EMI, InterestAmount, TotalAmount;
+        int  loanAmount, tenure, EMI, interestAmount, totalAmount;
         double a,b,c,d,e;
-        float RateOfInterest;
+        float rateOfInterest;
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter your loan amount");
-        LoanAmount = sc.nextInt();
+        loanAmount = sc.nextInt();
         System.out.println("Please enter your Tenure in months only");
-        Tenure = sc.nextInt();
+        tenure = sc.nextInt();
         System.out.println("Please enter your Rate of Interest for the loan");
         a = sc.nextFloat();
-        RateOfInterest = (float) ((a/12)/100);
-        b = 1+RateOfInterest;
-        c = Math.pow(b,Tenure);
-        d = LoanAmount * RateOfInterest;
+        rateOfInterest = (float) ((a/12)/100);
+        b = 1+rateOfInterest;
+        c = Math.pow(b,tenure);
+        d = loanAmount * rateOfInterest;
         e = c / (c-1);
         EMI = (int) (d*e);
         System.out.println("Your monthly EMI is " + EMI);
-        System.out.println("Your Principle amount is " + LoanAmount);
-        TotalAmount = EMI*Tenure;
-        System.out.println("Your Total Amount is " + TotalAmount);
-        InterestAmount = TotalAmount-LoanAmount;
-        System.out.println("Interest Amount is " + InterestAmount);
+        System.out.println("Your Principle amount is " + loanAmount);
+        totalAmount = EMI*tenure;
+        System.out.println("Your Total Amount is " + totalAmount);
+        interestAmount = totalAmount-loanAmount;
+        System.out.println("Interest Amount is " + interestAmount);
     }
 }
--- MiniProjects/SIP_Calculator.java
+++ MiniProjects/SIP_Calculator.java
@@ -5,25 +5,25 @@ import java.util.Scanner;
 
 public class SIP_Calculator {
     public static void main(String[] args) {
-        int MonthlyInvestment,TimePeriod, InvestedAmount,EstimatedReturns,TotalValue;
-        float ReturnRate, a, b, c, d ;
+        int monthlyInvestment,timePeriod, investedAmount,estimatedReturns,totalValue;
+        float returnRate, a, b, c, d ;
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter your monthly investment");
-        MonthlyInvestment = sc.nextInt();
+        monthlyInvestment = sc.nextInt();
         System.out.println("Please enter your Time period in years only");
-        TimePeriod = sc.nextInt();
+        timePeriod= sc.nextInt();
         System.out.println("Please enter your expected Return Rate");
         a = sc.nextFloat();
-        ReturnRate = (a/12)/100;
-        b = 1+ ReturnRate;
-        c= (float) (Math.pow(b,(TimePeriod*12)));
+        returnRate = (a/12)/100;
+        b = 1+ returnRate;
+        c= (float) (Math.pow(b,(timePeriod*12)));
         System.out.println(c);
-        d = (c-1)/ReturnRate;
-        TotalValue= (int) (MonthlyInvestment * d * b);
-        System.out.println("Your Total Value is " + TotalValue);
-        InvestedAmount = MonthlyInvestment * (TimePeriod * 12);
-        System.out.println("Your Invested Amount is " + InvestedAmount);
-        EstimatedReturns = TotalValue - InvestedAmount;
-        System.out.println("Your Estimated Returns are " + EstimatedReturns);
+        d = (c-1)/returnRate;
+        totalValue= (int) (monthlyInvestment * d * b);
+        System.out.println("Your Total Value is " + totalValue);
+        investedAmount = monthlyInvestment * (timePeriod * 12);
+        System.out.println("Your Invested Amount is " + investedAmount);
+        estimatedReturns = totalValue - investedAmount;
+        System.out.println("Your Estimated Returns are " + estimatedReturns);
     }
 }
